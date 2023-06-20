acedaaae89c19effdf542ac18a75a83edca7b8dd
denis-adelin <denis.bardas@student.unitbv.ro>
2023-06-20 15:55:26 +0300

    Added fragments and menu to navigate through them

--- app/src/main/java/com/example/challengeapp/AppActivity.kt
+++ app/src/main/java/com/example/challengeapp/AppActivity.kt
@@ -2,10 +2,19 @@ package com.example.challengeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class AppActivity:AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController=navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        setupWithNavController(bottomNavigationView, navController)
     }
 }
---
+++ app/src/main/java/com/example/challengeapp/fragments/AddPostFragment.kt
@@ -0,0 +1,23 @@
+package com.example.challengeapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.challengeapp.R
+
+class AddPostFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_post, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
