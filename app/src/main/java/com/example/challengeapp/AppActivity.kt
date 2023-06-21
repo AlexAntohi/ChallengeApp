98ac4bcc93c3e57224fe86a673704d4345676150
denis-adelin <denis.bardas@student.unitbv.ro>
2023-06-21 10:26:06 +0300

    Added options menu that lets you change the theme between dark and light mode and to log out

--- app/src/main/java/com/example/challengeapp/AppActivity.kt
+++ app/src/main/java/com/example/challengeapp/AppActivity.kt
@@ -1,7 +1,12 @@
 package com.example.challengeapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
@@ -16,5 +21,33 @@ class AppActivity:AppCompatActivity() {
         navController=navHostFragment.navController
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         setupWithNavController(bottomNavigationView, navController)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.light_mode -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                true
+            }
+            R.id.dark_mode -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                true
+            }
+            R.id.log_out -> {
+                logout()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    private fun logout()
+    {
+        val intent = Intent(this@AppActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
